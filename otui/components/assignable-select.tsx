6e797a2dd792e2b203f8c1ad955a4a85e37aeafc
Hadi Hamoud <hadikhamoud@gmail.com>
2025-08-29 10:59:40 +0300

    attempting assignable select

---
+++ otui/components/assignable-select.tsx
@@ -0,0 +1,40 @@
+import { SelectRenderable } from "@opentui/core"
+import { extend } from "@opentui/react"
+
+class ExtendedSelectRenderable extends SelectRenderable {
+  constructor(id: string, options: any) {
+    super(id, options)
+
+    if (options.value !== undefined) {
+      this.value = options.value
+    }
+  }
+
+  get value(): any {
+    const selected = this.getSelectedOption()
+    return selected ? selected.value : undefined
+  }
+
+  set value(val: any) {
+    const idx = this.options.findIndex(opt => opt.value === val)
+    if (idx >= 0) {
+      this.setSelectedIndex(idx)
+    }
+  }
+}
+
+declare module "@opentui/react" {
+  interface OpenTUIComponents {
+    select: typeof ExtendedSelectRenderable
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      select: any
+    }
+  }
+}
+
+extend({ select: ExtendedSelectRenderable })
