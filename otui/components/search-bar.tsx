1a0d5bcef32b1f3f6bd234da3175f5d89631f9a8
Hadi Hamoud <hadikhamoud@gmail.com>
2025-09-02 18:54:58 +0300

    added search bar

---
+++ otui/components/search-bar.tsx
@@ -0,0 +1,21 @@
+import { useAppStore } from "../store/app-store"
+
+export function SearchBar() {
+  const { 
+    isSearchActive, 
+    searchQuery,
+    focusedBox
+  } = useAppStore()
+
+  if (!isSearchActive) {
+    return null
+  }
+
+  return (
+    <group width="100%" height={1}>
+      <text>
+        Search {focusedBox}: {searchQuery}
+      </text>
+    </group>
+  )
+}
