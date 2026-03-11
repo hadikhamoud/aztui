e16da81fc725084e75dce99d91fcbbec853d34ac
Hadi Hamoud <hadikhamoud@gmail.com>
2026-03-11 23:57:13 +0300

    removed comments

--- commands/login.ts
+++ commands/login.ts
@@ -2,13 +2,9 @@ import { getCredentials } from "../config"
 import { useAppStore } from "../store/app-store"
 
 export async function runLoginCommand(): Promise<void> {
-  // Get existing credentials to pre-populate the org URL
   const existingCredentials = getCredentials()
-  
-  // Force the setup view to show, pre-populate with existing org URL
-  // Don't pre-fill PAT for security reasons
-  // Credentials are NOT deleted - they remain valid until new ones are saved
-  useAppStore.setState({ 
+
+  useAppStore.setState({
     needsSetup: true,
     setupOrgUrl: existingCredentials?.orgUrl || '',
     setupPat: '',
